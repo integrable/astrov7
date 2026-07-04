6a137b2992359af06e1f3f58eb4d24eadc6d6e4a
integrable <renphysics@gmail.com>
2026-07-04 17:59:17 +0800

    Update astro.config.mjs
    customCss: ['katex/dist/katex.min.css']

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
-			customCss: ['./src/styles/katex.css'],
+			customCss: ['katex/dist/katex.min.css'],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
